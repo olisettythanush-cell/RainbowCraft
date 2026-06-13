c20ae1821a3193f44da00e6db9c774994b6f1f69
olisettythanush-cell <olisettythanush@gmail.com>
2026-06-13 18:14:50 +0530

    Create RainbowCraft class for mod initialization

---
+++ src/main/java/username/rainbowcraft/RainbowCraft.java
@@ -0,0 +1,12 @@
+package com.yourname.rainbowcraft;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(RainbowCraft.MOD_ID)
+public class RainbowCraft {
+    public static final String MOD_ID = "rainbowcraft";
+
+    public RainbowCraft() {
+        System.out.println("RainbowCraft loaded!");
+    }
+}
